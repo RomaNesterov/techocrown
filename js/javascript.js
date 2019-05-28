d127202aa294deb55c77cabbbbb9854ae9c9e6e3
RomaNesterov <r.nesterow2013@yandex.ru>
2019-05-28 13:21:20 +0300

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+
+    $('.pull-right').on("click", function () {
+
+        $(this).parents('.blocklist').find('ul').toggle(300).css('font-weight','bolder');
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
